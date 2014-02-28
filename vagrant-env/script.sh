81a109e40983166a39d5ee57bd9812a47106eb61
Naotaka Jay Hotta <fukuzo@cybertron.co.jp>
2014-02-28 18:22:47 +0900

    script update

--- vagrant-env/script.sh
+++ vagrant-env/script.sh
@@ -4,10 +4,10 @@ sudo apt-get update
 sudo apt-get -y install golang git mercurial sqlite language-pack-en-base language-pack-ja-base
 # sudo dpkg-reconfigure locales
 
-mkdir /home/vagrant/gocode
+echo "export GOPATH=/home/vagrant/gocode" >> .bashrc
+echo "export PATH=$PATH:/home/vagrant/gocode/bin" >> .bashrc
 
 export GOPATH=/home/vagrant/gocode
-echo 'export GOPATH=/home/vagrant/gocode' >> .bashrc
+mkdir /home/vagrant/gocode
 cd /home/vagrant/gocode && go get github.com/robfig/revel
 cd /home/vagrant/gocode && go get github.com/robfig/revel/revel
-echo 'export PATH=$PATH:$GOPATH/bin' >> .bashrc
