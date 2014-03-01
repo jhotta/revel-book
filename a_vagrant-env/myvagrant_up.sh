e2a9fdd6b5bb5f62c017c52b1f91d2ecabe281e8
Jay Hotta <j-hotta@creationline.com>
2014-03-01 13:29:34 +0900

    update CLI scritp

--- a_vagrant-env/myvagrant_up.sh
+++ a_vagrant-env/myvagrant_up.sh
@@ -1,8 +1,14 @@
 #! /bin/sh
 
-if [ -e ~/.ssh/known_hosts ]; then
-	rm ~/.ssh/known_hosts
-	touch ~/.ssh/known_hosts
-fi
+#if []; then
+#	if [ -e ~/.ssh/known_hosts ]; then
+#		rm ~/.ssh/known_hosts
+#		touch ~/.ssh/known_hosts
+#	fi
+#elfi []; then
+sed -e "/\[127.0.0.1\]\:2222/d" ~/.ssh/known_hosts > tmpfile
+mv tmpfile ~/.ssh/known_hosts
+
+#fi
 
 vagrant up
