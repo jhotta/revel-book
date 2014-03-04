d2d4616604f33198b6644f3b0c6519ec0557e2a6
Jay Hotta <j-hotta@creationline.com>
2014-03-04 15:47:24 +0900

    update CLI script

--- s_vagrant-env/script.sh
+++ s_vagrant-env/script.sh
@@ -1,18 +1,22 @@
 #! /bin/sh
 
-# DNSの調子が悪い時にセットする。
+# DNSでhost名からIPアドレスの検索が出来てないときのためにセットする。
 echo "91.189.92.200 archive.ubuntu.com" >> /etc/hosts
 echo "91.189.92.200 security.ubuntu.com" >> /etc/hosts
 echo "192.30.252.131 github.com" >> /etc/hosts
 echo "74.125.235.129 code.google.com" >> /etc/hosts
 
+# Ubuntuパッケージのインストール
 sudo apt-get update
 sudo apt-get -y install git mercurial sqlite libsqlite3-dev golang language-pack-en-base language-pack-ja-base tree
 # sudo dpkg-reconfigure locales
 
+# gocodeへのパスの設定
 echo "export GOPATH=/home/vagrant/gocode" >> .bashrc
 echo "export PATH=$PATH:/home/vagrant/gocode/bin" >> .bashrc
 
+
+# revelを実行するための環境をインポート
 export GOPATH=/home/vagrant/gocode
 mkdir /home/vagrant/gocode
 cd /home/vagrant/gocode && go get github.com/robfig/revel
@@ -22,4 +26,5 @@ cd /home/vagrant/gocode && go get github.com/mattn/go-sqlite3
 cd /home/vagrant/gocode && go get github.com/robfig/revel/revel
 cd /home/vagrant/gocode/src && git clone https://github.com/jhotta/revelFramework4Go.git 
 
+# gocodeディレクトリーの所有権を変更
 chown -R vagrant:vagrant /home/vagrant/gocode
